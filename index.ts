daf06f493a39a53b4492bb8a20c0dae395e5c2ee
Noah Labrecque <noah.labrecque@gmail.com>
2026-02-01 12:17:56 -0500

    exports

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import Counter from "./src/metric/counter";
+import Gauge from "./src/metric/gauge";
+import Histogram from "./src/metric/histogram";
+import Observer from "./src/metric/observer";
+
+import MetricRegistry from "./src/collector_registry";
+import Metric from "./src/metric/metric";
+
+export { Counter, Gauge, Histogram, Observer };
+export { Metric, MetricRegistry };
+
+export default MetricRegistry.DEFAULT_REGISTRY;
